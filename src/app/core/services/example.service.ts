340934ddfa29fbd4158e463139bd05a8e293e1e9
paul-ventura-git <j.paul.ventura@gmail.com>
2026-03-26 21:09:32 -0500

    03

--- src/app/core/services/example.service.ts
+++ src/app/core/services/example.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ export class ExampleService {
   constructor(private http: HttpClient) { }
 
   // getAll()
-  getUsers() {
-    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>('https://jsonplaceholder.typicode.com/users');
   }
 
   // getById()
